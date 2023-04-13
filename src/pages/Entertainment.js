c19dd2805635821a59264456110a29446d5a660f
Bhupesh <124655980+bhupeshcuvette@users.noreply.github.com>
2023-04-13 11:48:25 +0530

    Add files via upload

---
+++ src/pages/Entertainment.js
@@ -0,0 +1,12 @@
+import Category from "../components/Entertainment/Category";
+
+const Entertainment =()=>{
+    return (
+        <div>
+        <Category/>
+        </div>
+    )
+
+}
+
+export default Entertainment;
