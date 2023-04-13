46000c5cbed7d4328c72f5b50715c357daabf405
Bhupesh <124655980+bhupeshcuvette@users.noreply.github.com>
2023-04-13 11:51:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Register from "./pages/Register";
+import Entertainment from "./pages/Entertainment";
+import Homepage from "./pages/Homepage";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +11,8 @@ function App() {
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Register/>}/>
+      <Route path="/entertainment" element={<Entertainment/>}/>
+      <Route path="/homepage" element={<Homepage/>}/>
       </Routes>
       </BrowserRouter>
       
