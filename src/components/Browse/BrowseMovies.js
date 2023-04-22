79c3e0b88f1f9976b29a5ca736422f0967764113
Bhupesh <124655980+bhupeshcuvette@users.noreply.github.com>
2023-04-22 22:29:16 +0530

    added toggle between browse and homepage

--- src/components/Browse/BrowseMovies.js
+++ src/components/Browse/BrowseMovies.js
@@ -1,11 +1,19 @@
 import React , {useState,useEffect} from "react";
-import GenerateMovies from "./GenerateMovies"
+import GenerateMovies from "./GenerateMovies";
+import ToggleBtn from "../../images/toggle-btn.png";
+import { useNavigate } from "react-router-dom";
+
 import "./BrowseMovies.css";
 
 const BrowseMovies=()=>{
+    const donavigate=useNavigate();
+    const handleClick=()=>{
+        donavigate("/homepage");
+    }
     const prevSelCatagories = JSON.parse(window.localStorage.getItem("entertainmentData"));
     return(
         <div className="all-movies">
+            <img src={ToggleBtn} className="t-button" onClick={handleClick}></img>
             <p className="sup-app">Super app</p>
             <p className="choice-text">Entertainment according to your choice</p>
             <div className="display-all-movies">
